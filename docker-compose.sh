1339cfe2a29f9626c37adfb63a5ef912ae0eaa4c
Igor Bari <mucsi96@gmail.com>
2024-04-27 19:09:37 +0000

    fix docker network

---
+++ docker-compose.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#https://www.authelia.com/reference/guides/generating-secure-values/#generating-an-rsa-keypair
+openssl genrsa -out /tmp/test_jwks.pem 4096
+# openssl rsa -in /tmp/test_jwks.pem -pubout -outform PEM | base64 | tr -d '\n' >> /tmp/test_jwks.pub
+export TEST_JWKS_KEY=$(cat /tmp/test_jwks.pem)
+
+output=$(docker run authelia/authelia:latest authelia crypto hash generate argon2 --random --random.length 5 --random.charset alphanumeric)
+echo "$output" | grep -oP '(?<=Random Password: ).*' > /tmp/test_password
+echo "$output" | grep -oP '(?<=Digest: ).*' > /tmp/test_password_hash
+export TEST_PASSWORD=$(cat /tmp/test_password)
+export TEST_PASSWORD_HASH=$(cat /tmp/test_password_hash)
+
+jinja2 docker-compose.j2.yml > docker-compose.rj2.yml
+jinja2 test/authelia_configuration.j2.yml > test/authelia_configuration.rj2.yml
+jinja2 test/authelia_users.j2.yml > test/authelia_users.rj2.yml
+
+docker-compose --file docker-compose.rj2.yml up --detach --build --force-recreate --wait --remove-orphans --pull always
+
+echo "172.16.238.10 dashboard.auth-tools.home" | sudo tee -a /etc/hosts
+echo "172.16.238.10 authelia.auth-tools.home" | sudo tee -a /etc/hosts
+echo "172.16.238.10 auth-tools.home" | sudo tee -a /etc/hosts
--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,17 +1,28 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+def get_password():
+    with open("/tmp/test_password") as f:
+        return f.read().strip()
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+def test_redirects_to_authelia_on_sign_in_click(page: Page):
+    page.goto("http://auth-tools.home")
+    page.get_by_role("button", name="Sign in").click()
+    expect(page).to_have_title("Login - Authelia")
+
+def test_shows_user_details_after_sign_in(page: Page):
+    page.goto("http://auth-tools.home")
+    page.get_by_role("button", name="Sign in").click()
+    page.get_by_label("Username").fill("john_doe")
+    page.keyboard.press("Tab")
+    page.get_by_label("Password").fill(get_password())
+    page.keyboard.press("Enter")
+    # page.get_by_role("button", name="SIGN IN").click()
+    expect(page).to_have_title("Logine - Authelia")
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
-
-    # Expects page to have a heading with the name of Installation.
+    page.goto("http://authelia.auth-tools.home")
+    page.get_by_label("Username").fill("john_doe")
+    page.get_by_label("Password").fill(get_password())
+    page.get_by_role("button", name="SIGN IN").click()
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
