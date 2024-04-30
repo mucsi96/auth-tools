2a649866f5d92daf8e00c86586985592b537baa2
Igor Bari <mucsi96@gmail.com>
2024-04-30 18:49:02 +0000

    use yml config for token handler

--- token_handler/src/clientConfig.ts
+++ token_handler/src/clientConfig.ts
@@ -1,12 +1,13 @@
 import assert from 'assert';
 import { readFileSync } from 'fs';
 import { Client } from 'oauth4webapi';
+import { load as parseYaml } from 'js-yaml';
 
-const clients: {
+const clients = parseYaml(readFileSync('/config/clients.yml', 'utf8')) as {
   client_id: string;
   client_secret: string;
   redirect_uris: string[];
-}[] = JSON.parse(readFileSync('/config/clients.json', 'utf8'));
+}[];
 
 export function getClientConfig(referer?: string): Client {
   assert(referer, 'referer not provided');
