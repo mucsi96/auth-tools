7f223b83c283f8283684f8fc154ec185d8457b51
Igor Bari <mucsi96@gmail.com>
2024-06-03 22:18:38 +0200

    update user info on sign out

--- client_lib/src/UserInfo.ts
+++ client_lib/src/UserInfo.ts
@@ -1,28 +1,32 @@
 import { onElementConnected } from '@mucsi96/ui-elements';
 import { getUserInfo, signin, signout } from './authService';
 
-onElementConnected('[bt-user-info]', (element) => {
+function renderUserInfo(element: HTMLElement) {
   const userInfo = getUserInfo();
 
   if (userInfo.isSignedIn && 'userName' in userInfo) {
     const { userName, email, initials } = userInfo;
     element.innerHTML = `<button bt-avatar id="avatar" popovertarget="avatar-popover">
-    ${initials}
-  </button>
-  <div popover bt id="avatar-popover">
-    <ul bt-dropdown-menu>
-      <li bt-separated>
-        <p>${userName}</p>
-        <p>${email}</p>
-      </li>
-      <li><button type="button" id="sign-out">Sign out</button></li>
-    </ul>
-  </div>`;
+      ${initials}
+    </button>
+    <div popover bt id="avatar-popover">
+      <ul bt-dropdown-menu>
+        <li bt-separated>
+          <p>${userName}</p>
+          <p>${email}</p>
+        </li>
+        <li><button type="button" id="sign-out">Sign out</button></li>
+      </ul>
+    </div>`;
     element
       .querySelector('#sign-out')
-      ?.addEventListener('click', () => signout());
+      ?.addEventListener('click', () =>
+        signout().then(() => renderUserInfo(element))
+      );
   } else {
     element.innerHTML = `<button bt>Sign in</button>`;
     element.querySelector('button')?.addEventListener('click', () => signin());
   }
-});
+}
+
+onElementConnected('[bt-user-info]', renderUserInfo);
