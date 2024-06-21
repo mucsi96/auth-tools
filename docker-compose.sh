bebf952dc5da08a7914a108bf903acf515bc79ee
Igor Bari <mucsi96@gmail.com>
2024-06-21 08:47:41 +0200

    populate all env variables

--- docker-compose.sh
+++ docker-compose.sh
@@ -13,6 +13,11 @@ if [[ -n "$CI" ]]; then
   echo "127.0.0.1 dashboard.auth-tools.home" | sudo tee -a /etc/hosts
   echo "127.0.0.1 auth.auth-tools.home" | sudo tee -a /etc/hosts
   echo "127.0.0.1 idp.auth-tools.home" | sudo tee -a /etc/hosts
+  export DEMO_API_CLIENT_ID="00000000-0000-0000-0000-000000000001"
+  export TRAEFIK_CLIENT_ID="00000000-0000-0000-0000-000000000002"
+  export CLIENT_ID="00000000-0000-0000-0000-000000000003"
+  export CLIENT_SECRET="test"
+  export TENANT_ID="auth-tools-tenant"
   docker compose up --detach --wait
 else
   docker compose up --detach --build --force-recreate --wait --remove-orphans --pull always
