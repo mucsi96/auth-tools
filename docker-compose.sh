1da38da4996f65ae853a31efc8c7116eca32b4af
Igor Bari <mucsi96@gmail.com>
2024-06-21 21:14:37 +0200

    try fixing docker copose

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-source .env
-docker compose version
-
-if [[ -n "$MOCK_IDENTITY_PROVIDER" ]]; then
+if [[ -n "$MOCK_IDENTITY_PROVIDER" || -n "$CI" ]]; then
   export TENANT_ID="auth-tools-tenant"
   export ISSUER="http://mock-identity-provider:8080/$TENANT_ID/v2.0"
   export DEMO_API_CLIENT_ID="00000000-0000-0000-0000-000000000001"
   export TRAEFIK_CLIENT_ID="00000000-0000-0000-0000-000000000002"
   export CLIENT_ID="00000000-0000-0000-0000-000000000003"
   export CLIENT_SECRET="test"
+else
+  source .env
 fi
+
+docker compose version
 
 if [[ -n "$CI" ]]; then
   echo "127.0.0.1 web.auth-tools.home" | sudo tee -a /etc/hosts
