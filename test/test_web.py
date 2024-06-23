34965305c76bd2b97bb2ead7873c569259cb8286
Igor Bari <mucsi96@gmail.com>
2024-06-23 09:34:16 +0200

    enable back all e2e tests

--- test/test_web.py
+++ test/test_web.py
@@ -8,7 +8,7 @@ def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
         "ignore_https_errors": True,
-        "record_har_path": "test-results/test.har"
+        "record_har_path": "test-results/test.har",
     }
 
 
@@ -21,45 +21,32 @@ def browser_type_launch_args(browser_type_launch_args):
     }
 
 
-# @fixture(scope="function")
-# def context(browser: Browser):
-#     context = browser.new_context()
-#     context.clear_cookies()
-#     yield context
-#     context.close()
+def test_restricts_access_by_showing_loging_page(page: Page):
+    page.goto("https://web.auth-tools.home")
+    expect(page.get_by_role("heading", name="Login", level=1)).to_be_visible()
+    sign_in(page, "user1")
+    expect(page.get_by_role("heading", name="Hello, World!", level=1)).to_be_visible()
 
 
-# @fixture(scope="function")
-# def page(context: Browser):
-#     page = context.new_page()
-#     yield page
-#     page.close()
+def test_keeps_user_logged_in_on_reload(page: Page):
+    page.goto("https://web.auth-tools.home")
+    expect(page.get_by_role("heading", name="Login", level=1)).to_be_visible()
+    sign_in(page, "user1")
+    page.reload()
+    expect(page.get_by_role("heading", name="Hello, World!", level=1)).to_be_visible()
 
 
-# def test_restricts_access_by_showing_loging_page(page: Page):
-#     page.goto("https://web.auth-tools.home")
-#     expect(page.get_by_role("heading", name="Login", level=1)).to_be_visible()
-#     sign_in(page, "user1")
-#     expect(page.get_by_role("heading", name="Hello, World!", level=1)).to_be_visible()
+def test_denies_access_to_user_without_permission(page: Page):
+    page.goto("https://web.auth-tools.home")
+    sign_in(page, "user2")
+    expect(page.get_by_text("Access denied")).to_be_visible()
 
 
-# def test_keeps_user_logged_in_on_reload(page: Page):
-#     page.goto("https://web.auth-tools.home")
-#     expect(page.get_by_role("heading", name="Login", level=1)).to_be_visible()
-#     sign_in(page, "user1")
-#     page.reload()
-#     expect(page.get_by_role("heading", name="Hello, World!", level=1)).to_be_visible()
-
-
-# def test_denies_access_to_user_without_permission(page: Page):
-#     page.goto("https://web.auth-tools.home")
-#     sign_in(page, "user2")
-#     expect(page.get_by_text("Access denied")).to_be_visible()
-
-
-# def test_denies_access_on_too_frequent_authentication(page: Page, context: BrowserContext):
-#     page.goto("https://web.auth-tools.home")
-#     sign_in(page, "user1")
-#     context.clear_cookies(name="traefik.accessToken")
-#     page.reload()
-#     expect(page.get_by_text("Access denied")).to_be_visible()
+def test_denies_access_on_too_frequent_authentication(
+    page: Page, context: BrowserContext
+):
+    page.goto("https://web.auth-tools.home")
+    sign_in(page, "user1")
+    context.clear_cookies(name="traefik.accessToken")
+    page.reload()
+    expect(page.get_by_text("Access denied")).to_be_visible()
