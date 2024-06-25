77e918fc9f255f8bb69a878bf690b25d6666b544
Igor Bari <mucsi96@gmail.com>
2024-06-25 08:46:50 +0200

    publish client lib

---
+++ scripts/publish_client_lib.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import sys
+from publish_tools import ansible_utils, version_utils, npm_utils
+
+root_directory = Path(__file__).parent.parent
+secrets = ansible_utils.load_vars(sys.argv[2], root_directory / "vars/vault.yaml")
+version = version_utils.get_version(src=root_directory, tag_prefix="client-lib")
+
+npm_utils.publish_npm_package(
+    src=root_directory / "client_lib",
+    version=version,
+    tag_prefix="client-lib",
+    npm_access_token=secrets["npm_access_token"],
+    github_access_token=sys.argv[1],
+)
